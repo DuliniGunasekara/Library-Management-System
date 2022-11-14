8f5f646ec6ffb4f32410d7fac8a72a7b3966b3e1
Duli96 <dulini@hsenidoutsourcing.com>
2022-11-14 16:46:01 +0530

    removed some unused content

--- src/main/java/com/example/librarymanagementsystem/constants/SecurityConstants.java
+++ src/main/java/com/example/librarymanagementsystem/constants/SecurityConstants.java
@@ -3,7 +3,5 @@ package com.example.librarymanagementsystem.constants;
 public class SecurityConstants {
     public static final String SECRET_KEY = "S6YszVBj+PQ8iSu+CStorw==";
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER = "Authorization";
     public static final String AUTHORIZATION_HEADER = "authorization_header";
-    public static final long TOKEN_VALIDITY = 900000L;
 }
